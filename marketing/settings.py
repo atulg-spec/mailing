041bba0fcc51ac24533470580e39c20e86458d7b
atul <atulg0736@gmail.com>
2024-09-18 03:39:40 +0530

    debug

--- marketing/settings.py
+++ marketing/settings.py
@@ -114,15 +114,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATIC_ROOT = 'static'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATIC_ROOT = 'static'
 MEDIA_ROOT = 'media'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
