b12d5d9165ac24f8f36a70e89986438e50610d95
atul <atulg0736@gmail.com>
2024-09-21 20:25:14 +0530

    media

--- marketing/settings.py
+++ marketing/settings.py
@@ -106,9 +106,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 STATIC_ROOT = 'static'
-MEDIA_ROOT = 'media'
+# MEDIA_ROOT = 'media'
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/media/')
+MEDIA_URL="/media/"
+
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
--- marketing/urls.py
+++ marketing/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
+from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
@@ -7,8 +8,14 @@ from django.contrib.sitemaps.views import sitemap
 
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('', admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
