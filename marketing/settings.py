84f7e31b721ae8dc0057a4cf630283ffbc02bb98
atul <atulg0736@gmail.com>
2024-09-21 21:10:49 +0530

    yo

--- marketing/settings.py
+++ marketing/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'django_ckeditor_5',
     'ckeditor_uploader',
     'dashboard',
